974e8c73bb7399cd028bf411743d4c8c0377ed25
Thomas Dietrich <tomriiich@gmail.com>
2021-03-24 15:28:43 +0100

    Mise à jour du mercredi 24

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 // Get the modal
-const modal = document.getElementById("myModal");
+const modals = document.querySelectorAll(".modal");
 
 // Get the button that opens the modal
-const btn = document.querySelector(".card-button");
+const buttons = document.querySelectorAll(".card-button");
 
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+for(let i=0; i<buttons.length; i++) {
+  buttons[i].onclick = function() {
+    modals[i].style.display = "block";
+  }
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+// Get the button that close the modal
+const close = document.querySelectorAll(".close");
+
+for(let i=0; i<close.length; i++) {
+  close[i].onclick = function() {
+    modals[i].style.display = "none";
+  }
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target.classList.contains("modal")) {
+    event.target.style.display = "none";
   }
 }
